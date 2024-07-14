2fe8603bbd810533c67ea6d081e40444647396d3
Abraamyan Alexander <samvelcc25@outlook.com>
2024-07-14 15:37:01 +0300

    rezise event updated

--- src/view/CustomGraphicsView.cpp
+++ src/view/CustomGraphicsView.cpp
@@ -8,6 +8,8 @@ CustomGraphicsView::CustomGraphicsView(QGraphicsScene *scene, QWidget *parent)
 void CustomGraphicsView::resizeEvent(QResizeEvent *event) {
     QGraphicsView::resizeEvent(event);
     if (scene()) {
-        scene()->setSceneRect(0, 0, event->size().width(), event->size().height());
+        QRectF newSceneRect(0, 0, event->size().width(), event->size().height());
+        scene()->setSceneRect(newSceneRect);
+        fitInView(newSceneRect, Qt::KeepAspectRatio); // Optional: Fit the view to the scene rect
     }
 }
